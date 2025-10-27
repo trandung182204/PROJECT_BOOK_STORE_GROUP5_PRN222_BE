0c28b4f623404be2882c4c1c4f4c5abc6b87fc32
nampvhhe180065 <phanvanhoainam04@gmail.com>
2025-10-27 22:16:31 +0700

    Reapply "Merge pull request #4 from hieplee107/hieple"
    This reverts commit 5c2c3b3d7c3100898b3baf4cbce8c0b8fc3456ff.

---
+++ Repositories/IAdminRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace PROJECT_BOOK_STORE_GROUP5_PRN222.Repositories
+{
+    public interface IAdminRepository
+    {
+        Task<object> GetSummaryAsync();
+        Task<IEnumerable<object>> GetSalesReportAsync(string type, DateTime? from, DateTime? to);
+        Task<IEnumerable<object>> GetTopCustomersAsync(int top);
+        Task<IEnumerable<object>> GetTopBooksAsync(int top);
+        Task<object> GetDeletedItemsAsync();
+        Task<bool> RestoreAsync(string table, long id);
+        Task<bool> RestoreUserAsync(string userId);
+    }
+}
